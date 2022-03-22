8b1dbb333ae908503e4330b2a8608c9ea8687158
dltmdghks1015 <ehdrb8408@naver.com>
2022-03-22 22:38:37 +0900

    first commit

---
+++ Pythonpratice.py
@@ -0,0 +1,8 @@
+english = int(input("input your english Score : "))
+math = int(input("input your math Score : "))
+korean = int(input("input your korean Score : "))
+
+sum = english+math+korean
+average = sum/3
+
+print("평균점수 = ",average)
